f734633a2ff796bc63b3051f5f96d83ff4866b66
nayyara-airlangga <nayyara.airlangga@gmail.com>
2021-08-24 16:38:17 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# knowyourweather
+
+A web app to show the current weather all around the world. Design is not mine.
+
+## Development
+
+The front-end are just a combination of `EJS` and `CSS`, while the back-end is constructed using `Node.js`
+I use the OpenWeatherMap API to fetch the weather data and with a bit of wizardry, it becomes like this.
---
+++ app.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+const config = require("./config");
+
+const app = express();
+
+let cityList = [];
+let message = "";
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(process.env.PORT || 3000, function () {
+  let port = process.env.PORT || 3000;
+  console.log(`Server is running on port ${port}`);
+});
+
+app.get("/", function (req, res) {
+  res.render("weather", { cityList: cityList, message: "" });
+});
+
+app.get("/page-not-found", function (req, res) {
+  res.render("error");
+});
+
+app.post("/", function (req, res) {
+  const query = req.body.cityName;
+  const apiKey = config.MY_API_KEY;
+  const units = "metric";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${apiKey}&units=${units}`;
+
+  https.get(url, function (apiRes) {
+    console.log(apiRes.statusCode);
+    if (apiRes.statusCode === 404 || apiRes.statusCode === 400) {
+      res.redirect("/page-not-found");
+    } else {
+      apiRes.on("data", function (data) {
+        const weatherData = JSON.parse(data);
+        const temp = weatherData.main.temp;
+        const icon = weatherData.weather[0].icon;
+        const countryID = weatherData.sys.country;
+        const description = weatherData.weather[0].description;
+        const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+
+        let weatherObject = {
+          cityName: weatherData.name,
+          temp: temp,
+          icon: imageURL,
+          description: description,
+          countryID: countryID,
+        };
+
+        if (cityList.length === 0) {
+          cityList.push(weatherObject);
+        } else {
+          if (
+            cityList.some(
+              (city) =>
+                city.cityName === weatherObject.cityName &&
+                city.countryID === weatherObject.countryID
+            )
+          ) {
+            message = `You've already requested for ${query}`;
+          } else {
+            cityList.push(weatherObject);
+          }
+        }
+
+        res.render("weather", {
+          cityList: cityList,
+          message: message,
+        });
+        message = "";
+      });
+    }
+  });
+});
+
+app.post("/page-not-found", function(req, res) {
+  res.redirect("/");
+});
